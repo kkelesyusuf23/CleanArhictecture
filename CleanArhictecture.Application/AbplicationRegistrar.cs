a18bd83dc27ea7e1f173aaddc36a8f2b1c3ee0cb
Yusuf Keles <kkelesyusuf23@gmail.com>
2025-02-15 05:46:21 +0300

    ...

---
+++ CleanArhictecture.Application/AbplicationRegistrar.cs
@@ -0,0 +1,26 @@
+﻿using CleanArhictecture.Application.Behaviors;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArhictecture.Application
+{
+    public static class AbplicationRegistrar
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(conf =>
+            {
+                conf.RegisterServicesFromAssembly(typeof(AbplicationRegistrar).Assembly);
+                conf.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            });
+
+            services.AddValidatorsFromAssembly(typeof(AbplicationRegistrar).Assembly);
+            return services;
+        }
+    }
+}
