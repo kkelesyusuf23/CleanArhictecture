15de550b49f3b1d9e9c57a1cf1568fe2df0b11a6
Yusuf Keles <kkelesyusuf23@gmail.com>
2025-02-15 06:16:59 +0300

    ...

---
+++ CleanArhictecture.Infrastructare/InfrastructureRegistrar.cs
@@ -0,0 +1,39 @@
+﻿using CleanArhictecture.Domain.Employees;
+using CleanArhictecture.Infrastructare.Context;
+using CleanArhictecture.Infrastructare.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Scrutor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArhictecture.Infrastructare
+{
+    public static class InfrastructureRegistrar
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(opt =>
+            {
+                string connectionString = configuration.GetConnectionString("SqlServer")!;
+                opt.UseSqlServer(connectionString);
+            });
+
+            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+
+            services.Scan(opt => opt
+            .FromAssemblies(typeof(InfrastructureRegistrar).Assembly)
+            .AddClasses(publicOnly: false)
+            .UsingRegistrationStrategy(RegistrationStrategy.Skip)
+            .AsImplementedInterfaces()
+            .WithScopedLifetime()
+            );
+
+            return services;
+        }
+    }
+}
---
+++ CleanArhictecture.Infrastructare/Repositories/EmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using CleanArhictecture.Domain.Employees;
+using CleanArhictecture.Infrastructare.Context;
+using GenericRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArhictecture.Infrastructare.Repositories
+{
+    internal sealed class EmployeeRepository : Repository<Employee, ApplicationDbContext>, IEmployeeRepository
+    {
+        public EmployeeRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
