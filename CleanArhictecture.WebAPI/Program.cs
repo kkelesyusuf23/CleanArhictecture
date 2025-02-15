af254fbefba14ec6844d4b3c24f3f01aacd11a34
Yusuf Keles <kkelesyusuf23@gmail.com>
2025-02-15 18:14:19 +0300

    ...

--- CleanArhictecture.WebAPI/Program.cs
+++ CleanArhictecture.WebAPI/Program.cs
@@ -1,5 +1,8 @@
 using CleanArhictecture.Infrastructare;
 using CleanArhictecture.Application;
+using Scalar.AspNetCore;
+using Microsoft.AspNetCore.RateLimiting;
+using System.Threading.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,8 +11,32 @@ builder.AddServiceDefaults();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
+builder.Services.AddCors();
+builder.Services.AddOpenApi();
+
+builder.Services.AddControllers();
+
+builder.Services.AddRateLimiter(x => x.AddFixedWindowLimiter("fixed",cfg =>
+{
+    cfg.QueueLimit = 100;
+    cfg.Window = TimeSpan.FromSeconds(1);
+    cfg.PermitLimit = 100;
+    cfg.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+}));
+
 var app = builder.Build();
 
+app.MapOpenApi();
+app.MapScalarApiReference();
+
 app.MapDefaultEndpoints();
+
+app.UseCors(x => x
+.AllowAnyHeader()
+.AllowCredentials()
+.AllowAnyMethod()
+.SetIsOriginAllowed(t => true));
+
+app.MapControllers().RequireRateLimiting("fixed");
 
 app.Run();
